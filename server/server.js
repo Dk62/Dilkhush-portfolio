aec07f9763422f27ff51341078545315e73d56fc
Dilkhush <dilkhushk922@gmail.com>
2026-05-28 15:27:49 +0530

    fix Express 5 wildcard routing syntax

--- server/server.js
+++ server/server.js
@@ -33,8 +33,8 @@ app.use('/api/upload', uploadRoutes);
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static(path.join(__dirname, '../client/dist')));
-  
-  app.get('(.*)', (req, res) => {
+
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
   });
 } else {
